84c90e4343868152708b6e832e734887ce5c9b15
Dan Green <danon0404@gmail.com>
2015-12-23 15:23:40 +0600

    uncompilify

--- lib.js
+++ lib.js
@@ -1,17 +1,30 @@
 var fs        = require('fs'),
 	path      = require('path'),
-	compilify = require('compilify');
+	through   = require('through2');
 
-var worker = fs.readFileSync(__dirname + "/node_modules/sass.js/dist/sass.worker.js", "utf8")
-	.replace(/"/g, '\\"');
+var worker;
 
-module.exports = compilify(function(file) {
+module.exports = function(file) {
+	return through(function(buf, enc, next) {
+		
+		var ext = path.extname(file),
+			source = buf.toString('utf8');
 
-	return generateTemplateModule(file);
+		if (ext == ".sasst" || ext == ".scsst" || ext == ".csst") {
 
-}, {
-	extensions: [".sasst", ".scsst"],
-});
+			generateTemplateModule(file).then(function(result) {
+				this.push(result);
+				next();
+			});
+			
+		} else {
+
+			this.push(source);
+			next();
+		}
+	});
+};
+
 
 function resolve(variables) {
 
@@ -32,29 +45,45 @@ function resolve(variables) {
 }
 
 function generateTemplateModule(styleTemplateString, ext) {
+	return new Promise(function(resolve) {
 
-	styleTemplateString = 
-	'"' + styleTemplateString
-		.replace(/"/g, '\\"')
-		.replace(/\n/g, "\\n")
-		.replace(/\$[\w\-\.]+/g, function(variable) {
-			return '" + resolve(variables, ' + variable.replace(/\$/, '"').replace(/[\.]/g, '", "') + '") + "';
-		}) + 
-	'"';
+		function generate() {
 
-	var moduleString = 
-		'var Sass = require("sass.js");' +
-		'if (!global.__sassWorkerUrl) {' +
-			'global.__sassWorkerUrl = URL.createObjectURL(new Blob(["' + worker + '"]));' +
-			'Sass.setWorkerUrl(global.__sassWorkerUrl);' +
-		'}' +
-		'var sass = new Sass();' +  
-		resolve + 
-		'module.exports = function(variables) {' +
-			'return new Promise(function(resolve) {' +
-				'sass.compile(' + styleTemplateString + ', function(result) { resolve(result.text) });' + 
-			'});' +
-		'};';
-	
-	return moduleString;
+			styleTemplateString = 
+			'"' + styleTemplateString
+				.replace(/"/g, '\\"')
+				.replace(/\n/g, "\\n")
+				.replace(/\$[\w\-\.]+/g, function(variable) {
+					return '" + resolve(variables, ' + variable.replace(/\$/, '"').replace(/[\.]/g, '", "') + '") + "';
+				}) + 
+			'"';
+
+			var moduleString = 
+				'var Sass = require("sass.js");' +
+				'if (!global.__sassWorkerUrl) {' +
+					'global.__sassWorkerUrl = URL.createObjectURL(new Blob(["' + worker + '"]));' +
+					'Sass.setWorkerUrl(global.__sassWorkerUrl);' +
+				'}' +
+				'var sass = new Sass();' +  
+				resolve + 
+				'module.exports = function(variables) {' +
+					'return new Promise(function(resolve) {' +
+						'sass.compile(' + styleTemplateString + ', function(result) { resolve(result.text) });' + 
+					'});' +
+				'};';
+			
+			resolve(moduleString);
+		}
+
+		if (typeof worker == "undefined") {
+
+			fs.readFile(require.resolve("sass.js/dist/sass.worker.js"), "utf8", function(err, file) {
+				worker = file.replace(/"/g, '\\"');
+				generate();
+			});
+
+		} else {
+			generate();
+		}
+	});
 }
