58f7f59759f5078d6ee1af4d34bf87742636c2d1
Dan Green <danon0404@gmail.com>
2016-01-14 14:05:41 +0600

    node hook

--- lib.js
+++ lib.js
@@ -1,51 +1,41 @@
-var path = require('path'),
+var fs   = require('fs'), 
+	path = require('path'),
 	sass = require('node-sass'),
 	through = require('through2');
 
-module.exports = function(file) {
-	return through(function(buf, enc, next) {
-		
-		var ext = path.extname(file),
-			source = buf.toString('utf8');
+function compileSass(source, indentedSyntax, sync) {
+	return new Promise(function(resolve, reject) {
 
-		if (ext == ".sasst" || ext == ".scsst") {
+		source = source.split(/\{\{|\}\}/)
+			.map(function(expr, i) {
 
-			source = source.split(/\{\{|\}\}/)
-				.map(function(expr, i) {
-
-					if (i % 2 == 0) {
-						return expr;
-					}
-
-					return '#{"[var__=' + expr.replace(/"/g, '\\"') + '__var]"}';
-				})
-				.join("");
-
-			var self = this;
-
-			sass.render({ data: source, indentedSyntax: ext == ".sasst" }, function(err, result) {
-
-				if (err) {
-					return next(err);
+				if (i % 2 == 0) {
+					return expr;
 				}
 
-				self.push(generateTemplateModule(result.css.toString('utf8')));
-				next();
-			});
+				return '#{"[var__=' + expr.replace(/"/g, '\\"') + '__var]"}';
+			})
+			.join("");
 
-		} else 
-		if (ext == ".csst") {
+		if (sync) {
 
-			this.push(generateTemplateModule(source));
-			next();
-			
+			var result = sass.renderSync({ data: source, indentedSyntax: indentedSyntax });
+
+			resolve(generateTemplateModule(result.css.toString('utf8')));
+
 		} else {
 
-			this.push(source);
-			next();
+			sass.render({ data: source, indentedSyntax: indentedSyntax }, function(err, result) {
+
+				if (err) {
+					return reject(err);
+				}
+
+				resolve(generateTemplateModule(result.css.toString('utf8')));
+			});
 		}
 	});
-};
+}
 
 function generateTemplateModule(cssTemplateString) {
 
@@ -67,3 +57,53 @@ function generateTemplateModule(cssTemplateString) {
 		'return "' + cssTemplateString + '";' +
 	'}';
 }
+
+module.exports = function(file) {
+	return through(function(buf, enc, next) {
+		
+		var ext = path.extname(file),
+			source = buf.toString('utf8');
+
+		if (ext == ".sasst" || ext == ".scsst") {
+
+			var self = this;
+
+			compileSass(source, ext == ".sasst")
+			.then(function(module) {
+				self.push(module);
+				next();
+			})
+			.catch(function(err) {
+				next(err);
+			});
+
+		} else 
+		if (ext == ".csst") {
+
+			this.push(generateTemplateModule(source));
+			next();
+			
+		} else {
+
+			this.push(source);
+			next();
+		}
+	});
+};
+
+module.exports.nodeHook = function() {
+
+	require.extensions['.sasst'] = function(module, filename) { 
+		compileSass(fs.readFileSync(filename, 'utf8'), true, true)
+		.then(eval); 
+	};
+
+	require.extensions['.scsst'] = function(module, filename) { 
+		compileSass(fs.readFileSync(filename, 'utf8'), false, true)
+		.then(eval); 
+	};
+
+	require.extensions['.csst'] = function(module, filename) { 
+		eval(generateTemplateModule(fs.readFileSync(filename, 'utf8'))); 
+	};
+};
